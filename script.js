5e1cc85dd5ed5f8ac268641d2da0a50d6aab8c10
anasmedhat162 <anasmedhat162@gmail.com>
2026-08-18 22:33:30 +0300

    Update website

--- script.js
+++ script.js
@@ -31,3 +31,45 @@ cards.forEach((card) => {
     observer.observe(card);
 
 });
+
+        // كود تفعيل التخزين الخارجي الذكي
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('sw.js')
+                    .then(reg => console.log('تم تسجيل نظام التشغيل بدون نت بنجاح!'))
+                    .catch(err => console.log('فشل التسجيل:', err));
+            });
+        }
+const CACHE_NAME = 'my-offline-site-v1';
+// ضع هنا كل الملفات التي تريد للموقع أن يحفظها ليعمل بدون نت
+const ASSETS_TO_CACHE = [
+    'index.html',
+    'manifest.json'
+];
+
+// تثبيت الملفات داخل جهاز المستخدم
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+// تفعيل النظام
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+// عرض الملفات المخزنة مباشرة إذا كان المستخدم غير متصل بالإنترنت
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse; // إذا كان الملف مخزن، افتحه فوراً
+            }
+            return fetch(event.request); // إذا لم يكن مخزن وجد نت، احلبه من السيرفر
+        })
+    );
+});
